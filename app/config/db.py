1dc7f99d681bce61a1fd0b50b507c176164ff9f9
Vishal Bhardwaj <vishalanshuman@gmail.com>
2026-07-06 17:25:04 +0530

    Refactor database configuration to load environment variables and update requirements.txt with additional dependencies

--- app/config/db.py
+++ app/config/db.py
@@ -1,9 +1,14 @@
 
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
+import os
 
-DATABASE_URL = "postgresql+asyncpg://postgres:root@localhost/grants_svc"
+load_dotenv()
 
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+print("--------------------------------->", DATABASE_URL)
 engine = create_async_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, class_=AsyncSession)
 Base = declarative_base()
